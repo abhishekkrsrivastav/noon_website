ea41be00f72851c5b343a91a1d1395f573a0ecd8
Abhishek  Srivastav <162480814+abhishekkrsrivastav@users.noreply.github.com>
2025-04-04 17:43:54 +0530

    enable cors globally

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const app = express();
 app.use(cors());
 
 const corsOptions = {
-    origin: ['http://localhost:3000','https://noon-website.onrender.com'],  
+    origin: ['http://localhost:3000', 'https://noon-website.onrender.com'],
     credentials: true
-  };
-  app.use(cors(corsOptions)); 
-
+};
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));  
 app.use(express.json());
 
 // routes for noon header
